3d446db317cb20a2f46a52232ca14b5b55f79d27
Tomas Oneto <tomas.m.oneto@gmail.com>
2017-10-29 22:50:25 -0300

    v0.0.1

---
+++ arduino.js
@@ -0,0 +1,75 @@
+const SerialPort = require('serialport');
+const { eachSeries } = require('async');
+const { get, find } = require('lodash');
+const { Duplex } = require('stream');
+
+class ArduinoStream extends Duplex {
+
+  constructor(options) {
+    super(options);
+    this.identifier = get(options, 'identifier', /arduino/i);
+
+    this.on('pipe', src => {
+      src.on('end', () => this.push(null));
+      src.on('close', (err) => this.emit('close', err));
+      src.on('error', (err) => this.emit('error', err));
+      src.on('open', () => this.emit('open'));
+    });
+  }
+
+  connect(cb) {
+    this.serialport = undefined;
+
+    let interval = setInterval(this._scan, 1000, (err, port) => {
+      if (err) return cb(err);
+
+      if (port) {
+        clearInterval(interval);
+        this.serialport = new SerialPort(port.comName, { baudRate: 9600 });
+        this.serialport.pipe(this);
+        return cb();
+      }
+    });
+
+    return interval;
+  }
+
+  _scan(cb) {
+    SerialPort.list((err, ports) => {
+      if (err) return cb(err);
+
+      let port = find(ports, p => p && p.pnpId && p.pnpId.match(this.identifier));
+      console.log(`Port: ${port.pnpId}`);
+
+      return cb(null, port);
+    });
+  }
+
+  // chunk <Buffer> | <string> | <any> The chunk to be written. Will always be a buffer unless the decodeStrings option was set to false or the stream is operating in object mode.
+  // encoding <string> If the chunk is a string, then encoding is the character encoding of that string. If chunk is a Buffer, or if the stream is operating in object mode, encoding may be ignored.
+  // callback <Function> Call this function (optionally with an error argument) when processing is complete for the supplied chunk
+  _write(chunk, encoding, callback) {
+    if (typeof chunk === 'string') chunk = Buffer.from(chunk, encoding);
+
+    this.serialport.write(chunk);
+    this.serialport.drain(callback);
+  }
+
+  // chunks <Array> The chunks to be written. Each chunk has following format: { chunk: ..., encoding: ... }.
+  // callback <Function> A callback function (optionally with an error argument) to be invoked when processing is complete for the supplied chunks.
+  _writev(chunks, callback) {
+    eachSeries(chunks, (item, cb) => { this._write(item.chunk, item.encoding, cb) }, callback);
+  }
+
+  // callback <Function> Call this function (optionally with an error argument) when finished writing any remaining data.
+  _final(callback) {
+    return callback();
+  }
+
+  // size <number> Number of bytes to read asynchronously
+  _read(size) {
+      // Do nothing
+  }
+}
+
+module.exports = ArduinoStream;
---
+++ server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const IO = require('socket.io');
+const { get } = require('lodash');
+
+const ArduinoStream = require('./arduino');
+
+class ArduinoServer {
+
+  static create(options) {
+    let port = get(options, 'port', 8080);
+    let server = http.createServer();
+    let io = IO(server);
+
+    io.arduino = new ArduinoStream();
+    io.arduino.on('data', data => io.emit('data', data));
+    io.arduino.on('open', () => {
+      io.arduino.connected = true;
+      io.emit('arduino connected');
+    });
+    io.arduino.on('error', err => io.emit('arduino error', err));
+    io.arduino.on('close', err => {
+      io.arduino.connected = false;
+      io.emit('arduino close', err)
+    });
+
+    io.on('connection', socket => {
+      if (io.arduino.connected) socket.emit('arduino connected');
+
+      socket.on('data', io.arduino.write);
+      socket.on('close', err => console.log(err, 'Client closed connection'));
+      socket.on('error', err => console.log(err));
+      socket.on('disconnect', (err) => console.log('user disconnected', err));
+    });
+
+    server.on('listening', () => console.log(`Server listening on ${port}`));
+
+    server.on('error', (err) => {
+      if (err.code == 'EADDRINUSE') {
+        console.warn('Address in use, retrying...');
+        setTimeout(() => {
+          server.close();
+          server.listen(port);
+        }, 1000);
+      }
+      else {
+        console.log(err);
+      }
+    });
+
+    server.on('close', (err) => console.log('Server closed', err));
+    server.listen(port);
+
+    io.arduino.connect(() => {});
+
+    return io;
+  }
+}
+
+module.exports = ArduinoServer;
